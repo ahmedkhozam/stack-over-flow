066143b815eed95df7e108fcad3ecff70869b5a1
Ahmed <ahmedkhozam8085@gmail.com>
2025-04-26 13:11:20 +0300

    Add StackUser Entity

---
+++ src/main/java/com/example/demo/entity/StackUser.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "stack_users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StackUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String bio;
+
+    private int reputation = 0;
+}
